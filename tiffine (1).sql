6e4df5acb04adb25f59cae0aad5b4f87b87c5166
phpworld <vinaysingh43@gmail.com>
2025-05-20 17:23:09 +0530

    database

--- tiffine (1).sql
+++ tiffine (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2025 at 01:10 PM
+-- Generation Time: May 20, 2025 at 01:52 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -46,6 +46,33 @@ INSERT INTO `admins` (`id`, `username`, `password`, `name`, `created_at`, `updat
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `subtitle` varchar(200) DEFAULT NULL,
+  `image` varchar(255) NOT NULL,
+  `button_text` varchar(50) DEFAULT NULL,
+  `button_link` varchar(255) DEFAULT NULL,
+  `order` int(11) NOT NULL DEFAULT 0,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `title`, `subtitle`, `image`, `button_text`, `button_link`, `order`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 'Tiffn Delight', 'Home Made Meals', '1747739157_f30e716f54a1b05ce6eb.webp', 'Order Now', '#', 0, 1, '2025-05-20 11:05:57', '2025-05-20 11:05:57'),
+(2, 'South Indian Home Meals', 'Order Online ', '1747739264_73fc4096220e04b305a9.png', 'Order Now', '#', 1, 1, '2025-05-20 11:07:44', '2025-05-20 11:07:44');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `bookings`
 --
 
@@ -57,6 +84,7 @@ CREATE TABLE `bookings` (
   `total_amount` decimal(10,2) NOT NULL DEFAULT 0.00,
   `status` enum('pending','confirmed','delivered','cancelled') NOT NULL DEFAULT 'pending',
   `payment_method` enum('wallet','cash') NOT NULL DEFAULT 'wallet',
+  `payment_id` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -65,13 +93,20 @@ CREATE TABLE `bookings` (
 -- Dumping data for table `bookings`
 --
 
-INSERT INTO `bookings` (`id`, `user_id`, `booking_date`, `delivery_slot_id`, `total_amount`, `status`, `payment_method`, `created_at`, `updated_at`) VALUES
-(8, 1, '2025-05-19', 1, 70.00, 'cancelled', 'cash', '2025-05-18 09:49:36', '2025-05-18 09:52:09'),
-(9, 1, '2025-05-19', 1, 80.00, 'delivered', 'wallet', '2025-05-18 10:02:36', '2025-05-18 10:05:39'),
-(10, 3, '2025-05-19', 1, 150.00, 'pending', 'cash', '2025-05-18 10:07:08', '2025-05-18 10:07:08'),
-(11, 1, '2025-05-19', 1, 150.00, 'confirmed', 'wallet', '2025-05-18 10:45:28', '2025-05-18 10:45:41'),
-(12, 3, '2025-05-19', 1, 220.00, 'pending', 'cash', '2025-05-18 10:48:32', '2025-05-18 10:48:32'),
-(13, 1, '2025-05-19', 1, 70.00, 'confirmed', 'wallet', '2025-05-18 10:49:53', '2025-05-18 10:50:31');
+INSERT INTO `bookings` (`id`, `user_id`, `booking_date`, `delivery_slot_id`, `total_amount`, `status`, `payment_method`, `payment_id`, `created_at`, `updated_at`) VALUES
+(8, 1, '2025-05-19', 1, 70.00, 'cancelled', 'cash', NULL, '2025-05-18 09:49:36', '2025-05-18 09:52:09'),
+(9, 1, '2025-05-19', 1, 80.00, 'delivered', 'wallet', NULL, '2025-05-18 10:02:36', '2025-05-18 10:05:39'),
+(10, 3, '2025-05-19', 1, 150.00, 'delivered', 'cash', NULL, '2025-05-18 10:07:08', '2025-05-19 05:27:34'),
+(11, 1, '2025-05-19', 1, 150.00, 'delivered', 'wallet', NULL, '2025-05-18 10:45:28', '2025-05-19 05:17:58'),
+(12, 3, '2025-05-19', 1, 220.00, 'delivered', 'cash', NULL, '2025-05-18 10:48:32', '2025-05-19 05:17:29'),
+(13, 1, '2025-05-19', 1, 70.00, 'delivered', 'wallet', NULL, '2025-05-18 10:49:53', '2025-05-19 05:18:04'),
+(14, 2, '2025-05-20', 1, 70.00, 'cancelled', 'cash', NULL, '2025-05-19 05:07:54', '2025-05-19 05:21:17'),
+(15, 3, '2025-05-20', 1, 220.00, 'delivered', 'wallet', NULL, '2025-05-19 05:16:32', '2025-05-19 05:17:21'),
+(16, 2, '2025-05-20', 1, 70.00, 'delivered', 'wallet', NULL, '2025-05-19 05:27:07', '2025-05-19 05:27:29'),
+(17, 2, '2025-05-20', 1, 80.00, 'cancelled', 'wallet', NULL, '2025-05-19 09:36:13', '2025-05-20 09:31:58'),
+(18, 1, '2025-05-21', 1, 70.00, 'delivered', 'wallet', NULL, '2025-05-20 06:09:53', '2025-05-20 06:11:10'),
+(19, 3, '2025-05-21', 1, 80.00, 'confirmed', 'cash', NULL, '2025-05-20 06:25:40', '2025-05-20 06:26:18'),
+(20, 2, '2025-05-21', 1, 70.00, 'cancelled', '', 'pay_QX7ljIRK2EXgH2', '2025-05-20 08:58:26', '2025-05-20 09:31:02');
 
 -- --------------------------------------------------------
 
@@ -103,7 +138,16 @@ INSERT INTO `booking_items` (`id`, `booking_id`, `dish_id`, `quantity`, `price`,
 (16, 12, 1, 1, 70.00, '2025-05-18 10:48:32', '2025-05-18 10:48:32'),
 (17, 12, 2, 1, 70.00, '2025-05-18 10:48:32', '2025-05-18 10:48:32'),
 (18, 12, 3, 1, 80.00, '2025-05-18 10:48:32', '2025-05-18 10:48:32'),
-(19, 13, 1, 1, 70.00, '2025-05-18 10:49:53', '2025-05-18 10:49:53');
+(19, 13, 1, 1, 70.00, '2025-05-18 10:49:53', '2025-05-18 10:49:53'),
+(20, 14, 1, 1, 70.00, '2025-05-19 05:07:54', '2025-05-19 05:07:54'),
+(21, 15, 2, 1, 70.00, '2025-05-19 05:16:32', '2025-05-19 05:16:32'),
+(22, 15, 1, 1, 70.00, '2025-05-19 05:16:32', '2025-05-19 05:16:32'),
+(23, 15, 3, 1, 80.00, '2025-05-19 05:16:32', '2025-05-19 05:16:32'),
+(24, 16, 1, 1, 70.00, '2025-05-19 05:27:07', '2025-05-19 05:27:07'),
+(25, 17, 3, 1, 80.00, '2025-05-19 09:36:13', '2025-05-19 09:36:13'),
+(26, 18, 1, 1, 70.00, '2025-05-20 06:09:53', '2025-05-20 06:09:53'),
+(27, 19, 3, 1, 80.00, '2025-05-20 06:25:40', '2025-05-20 06:25:40'),
+(28, 20, 2, 1, 70.00, '2025-05-20 08:58:26', '2025-05-20 08:58:26');
 
 -- --------------------------------------------------------
 
@@ -151,8 +195,8 @@ CREATE TABLE `dishes` (
 
 INSERT INTO `dishes` (`id`, `name`, `description`, `price`, `image`, `available`, `is_vegetarian`, `created_at`, `updated_at`) VALUES
 (1, 'Rice + Dal + Roti + Sabji ', 'Rice + Dal + Roti + Sabji ', 70.00, '1747479007_0f8ba2bca1f0112e6c8f.jpeg', 1, 1, '2025-05-16 13:00:11', '2025-05-17 10:50:07'),
-(2, 'Egg Curry + Roti + Rice', 'Egg Curry + Roti + Rice', 70.00, '1747478985_427ef2d30e06c73181c7.jpg', 1, 0, '2025-05-16 13:01:11', '2025-05-17 11:36:48'),
-(3, 'Sahi Paneer + Roti 4 + Salad ', 'Sahi Paneer + Roti 4 + Salad ', 80.00, '1747478891_78cd1375a89b3368b3a6.jpg', 1, 1, '2025-05-17 10:48:11', '2025-05-17 10:48:11');
+(2, 'Egg Curry + Roti + Rice', 'Egg Curry + Roti + Rice', 70.00, '1747478985_427ef2d30e06c73181c7.jpg', 1, 0, '2025-05-16 13:01:11', '2025-05-20 06:12:15'),
+(3, 'Sahi Paneer + Roti 4 + Salad ', 'Sahi Paneer + Roti 4 + Salad ', 80.00, '1747478891_78cd1375a89b3368b3a6.jpg', 1, 1, '2025-05-17 10:48:11', '2025-05-19 05:14:49');
 
 -- --------------------------------------------------------
 
@@ -177,7 +221,12 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
 (1, '2025-05-16-113610', 'App\\Database\\Migrations\\CreateTiffineTables', 'default', 'App', 1747395452, 1),
 (2, '2023_06_15_000001', 'App\\Database\\Migrations\\CreateReviewsTable', 'default', 'App', 1747478435, 2),
-(3, '2025-05-17-112953', 'App\\Database\\Migrations\\AddVegetarianFieldToDishes', 'default', 'App', 1747481496, 3);
+(3, '2025-05-17-112953', 'App\\Database\\Migrations\\AddVegetarianFieldToDishes', 'default', 'App', 1747481496, 3),
+(4, '2023-11-15-123456', 'App\\Database\\Migrations\\UpdateWalletTransactions', 'default', 'App', 1747730662, 4),
+(5, '2023-11-15-123457', 'App\\Database\\Migrations\\UpdateWalletTransactionsData', 'default', 'App', 1747730789, 5),
+(6, '2023-11-15-123458', 'App\\Database\\Migrations\\AddPaymentIdToBookings', 'default', 'App', 1747730960, 6),
+(7, '2023-11-15-123459', 'App\\Database\\Migrations\\ModifyWalletTransactions', 'default', 'App', 1747734127, 7),
+(8, '2023-11-15-123460', 'App\\Database\\Migrations\\CreateBannersTable', 'default', 'App', 1747734127, 7);
 
 -- --------------------------------------------------------
 
@@ -194,6 +243,13 @@ CREATE TABLE `reviews` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `user_id`, `booking_id`, `rating`, `comment`, `created_at`, `updated_at`) VALUES
+(3, 2, 16, 5, 'Very Nice Meal', '2025-05-19 05:28:02', '2025-05-19 05:28:02');
 
 -- --------------------------------------------------------
 
@@ -240,9 +296,9 @@ CREATE TABLE `wallets` (
 --
 
 INSERT INTO `wallets` (`id`, `user_id`, `balance`, `created_at`, `updated_at`) VALUES
-(1, 1, 720.00, '2025-05-16 13:03:22', '2025-05-18 10:49:53'),
-(2, 2, 930.00, '2025-05-18 07:03:01', '2025-05-18 07:03:36'),
-(3, 3, 1600.00, '2025-05-18 07:07:19', '2025-05-18 09:37:59');
+(1, 1, 650.00, '2025-05-16 13:03:22', '2025-05-20 06:09:53'),
+(2, 2, 1180.00, '2025-05-18 07:03:01', '2025-05-20 09:31:58'),
+(3, 3, 1480.00, '2025-05-18 07:07:19', '2025-05-19 05:18:20');
 
 -- --------------------------------------------------------
 
@@ -252,7 +308,8 @@ INSERT INTO `wallets` (`id`, `user_id`, `balance`, `created_at`, `updated_at`) V
 
 CREATE TABLE `wallet_transactions` (
   `id` int(11) UNSIGNED NOT NULL,
-  `user_id` int(11) UNSIGNED NOT NULL,
+  `wallet_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `type` enum('credit','debit') NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -263,20 +320,22 @@ CREATE TABLE `wallet_transactions` (
 -- Dumping data for table `wallet_transactions`
 --
 
-INSERT INTO `wallet_transactions` (`id`, `user_id`, `type`, `amount`, `description`, `created_at`) VALUES
-(1, 1, 'credit', 100.00, 'Wallet Recharge', '2025-05-17 07:47:40'),
-(2, 1, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-17 07:48:13'),
-(3, 1, 'credit', 70.00, 'Refund for cancelled booking #2', '2025-05-17 07:49:12'),
-(4, 1, 'debit', 140.00, 'Payment for tiffin booking', '2025-05-17 07:50:26'),
-(5, 1, 'credit', 100.00, 'Wallet Recharge', '2025-05-17 09:33:19'),
-(6, 1, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-17 09:34:27'),
-(7, 1, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-17 09:48:17'),
-(8, 2, 'credit', 1000.00, 'Wallet Recharge', '2025-05-18 07:03:29'),
-(9, 2, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-18 07:03:36'),
-(10, 3, 'credit', 500.00, 'Wallet Recharge', '2025-05-18 07:07:30'),
-(11, 3, 'credit', 1000.00, 'Wallet Recharge', '2025-05-18 07:07:35'),
-(12, 1, 'credit', 1000.00, 'Wallet Recharge', '2025-05-18 10:02:01'),
-(13, 1, 'debit', 80.00, 'Payment for tiffin booking', '2025-05-18 10:02:36');
+INSERT INTO `wallet_transactions` (`id`, `wallet_id`, `user_id`, `type`, `amount`, `description`, `created_at`) VALUES
+(1, 1, 1, 'credit', 100.00, 'Wallet Recharge', '2025-05-17 07:47:40'),
+(2, 1, 1, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-17 07:48:13'),
+(3, 1, 1, 'credit', 70.00, 'Refund for cancelled booking #2', '2025-05-17 07:49:12'),
+(4, 1, 1, 'debit', 140.00, 'Payment for tiffin booking', '2025-05-17 07:50:26'),
+(5, 1, 1, 'credit', 100.00, 'Wallet Recharge', '2025-05-17 09:33:19'),
+(6, 1, 1, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-17 09:34:27'),
+(7, 1, 1, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-17 09:48:17'),
+(8, 2, 2, 'credit', 1000.00, 'Wallet Recharge', '2025-05-18 07:03:29'),
+(9, 2, 2, 'debit', 70.00, 'Payment for tiffin booking', '2025-05-18 07:03:36'),
+(10, 3, 3, 'credit', 500.00, 'Wallet Recharge', '2025-05-18 07:07:30'),
+(11, 3, 3, 'credit', 1000.00, 'Wallet Recharge', '2025-05-18 07:07:35'),
+(12, 1, 1, 'credit', 1000.00, 'Wallet Recharge', '2025-05-18 10:02:01'),
+(13, 1, 1, 'debit', 80.00, 'Payment for tiffin booking', '2025-05-18 10:02:36'),
+(17, 2, 2, 'credit', 1000.00, 'Wallet Recharge via Razorpay (Payment ID: pay_QX7jE7mLrjEWOV)', '2025-05-20 08:56:05'),
+(18, 2, 2, 'credit', 80.00, 'Refund for cancelled booking #17', '2025-05-20 09:31:58');
 
 --
 -- Indexes for dumped tables
@@ -288,6 +347,12 @@ INSERT INTO `wallet_transactions` (`id`, `user_id`, `type`, `amount`, `descripti
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `bookings`
@@ -363,16 +428,22 @@ ALTER TABLE `admins`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `booking_items`
 --
 ALTER TABLE `booking_items`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `delivery_slots`
@@ -390,13 +461,13 @@ ALTER TABLE `dishes`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -414,7 +485,7 @@ ALTER TABLE `wallets`
 -- AUTO_INCREMENT for table `wallet_transactions`
 --
 ALTER TABLE `wallet_transactions`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
@@ -446,12 +517,6 @@ ALTER TABLE `reviews`
 --
 ALTER TABLE `wallets`
   ADD CONSTRAINT `wallets_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `wallet_transactions`
---
-ALTER TABLE `wallet_transactions`
-  ADD CONSTRAINT `wallet_transactions_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
